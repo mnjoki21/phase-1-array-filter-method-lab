92774c5fb3796ce06c75e8e94f575738c5fadb9b
mitchelle <mitchellenjoki7@gmail.com>
2022-06-09 23:28:29 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers , taxi) {
+    return drivers.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === taxi.toLowerCase()
+    );
+  } 
+
+  function fuzzyMatch(drivers, oneTaxi) {
+    return drivers.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(oneTaxi.toLowerCase()) === 0
+    );
+  }
+  function matchName(drivers, matchName) {
+    return drivers.filter((record) => record.name === matchName);
+  }
+
